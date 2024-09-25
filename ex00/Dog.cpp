e592b9e443d131de1e09a5e3dd628e7183f2b8af
mstrauss <matthias.strauss@proton.me>
2024-09-25 18:12:51 +0200

    ex00 added Dog class

--- ex00/Dog.cpp
+++ ex00/Dog.cpp
@@ -6,8 +6,53 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:57:12 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/09/25 16:57:34 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/09/25 18:11:11 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
+
+
+#include "Dog.hpp"
+
+/* -------------------------------------------------------------------------- */
+/*                                Constructors                                */
+/* -------------------------------------------------------------------------- */
+
+Dog::Dog() : Animal("Dog")
+{
+	std::cout << "Dog created using default constructor" << std::endl;
+}
+
+Dog::Dog(const std::string &type) : Animal(type)
+{
+	std::cout << "Dog created using parameterized  constructor" << std::endl;
+}
+
+Dog::Dog(Dog const &src) : Animal(src)
+{
+	std::cout << "Dog created using copy constructor" << std::endl;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                Deconstructor                               */
+/* -------------------------------------------------------------------------- */
+Dog::~Dog()
+{
+	std::cout << "Dog destroyed" << std::endl;
+}
+
+/* ------------------------------ Copy operator ----------------------------- */
+Dog &Dog::operator=(Dog const &src)
+{
+	this->_type = src._type;
+	return *this;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   Actions                                  */
+/* -------------------------------------------------------------------------- */
+void Dog::makeSound() const
+{
+	std::cout << "Woof" << std::endl;
+}
--- ex00/Dog.hpp
+++ ex00/Dog.hpp
@@ -6,7 +6,24 @@
 /*   By: mstrauss <mstrauss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 16:57:10 by mstrauss          #+#    #+#             */
-/*   Updated: 2024/09/25 16:57:11 by mstrauss         ###   ########.fr       */
+/*   Updated: 2024/09/25 18:10:33 by mstrauss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef DOG_HPP
+#define DOG_HPP
+#include "Animal.hpp"
+
+class Dog : public Animal
+{
+public:
+	Dog();
+	Dog(const std::string &type);
+	Dog(Dog const &src);
+	virtual ~Dog();
+	Dog &operator=(Dog const &src);
+
+	void makeSound() const override;
+};
+
+#endif
